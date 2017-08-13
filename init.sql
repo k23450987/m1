211812a51c5f21142a88e2133eb9f2ff097266e2
k23450987 <xiaoti008@gmail.com>
2017-08-13 13:40:46 +0800

    初始化数据库

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Ultimate v11.11 (64 bit)
-MySQL - 5.5.24 : Database - m1
+SQLyog  v12.2.6 (64 bit)
+MySQL - 5.5.27 : Database - m1
 *********************************************************************
 */
 
@@ -12,7 +12,7 @@ MySQL - 5.5.24 : Database - m1
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`m1` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`m1` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `m1`;
 
@@ -21,70 +21,85 @@ USE `m1`;
 DROP TABLE IF EXISTS `colinfo`;
 
 CREATE TABLE `colinfo` (
-  `colid` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) COLLATE utf8_bin DEFAULT NULL,
-  `type` varchar(30) COLLATE utf8_bin DEFAULT NULL,
-  `unique` varchar(30) COLLATE utf8_bin DEFAULT NULL,
-  `tableId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`colid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `COLID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(30) COLLATE utf8_bin DEFAULT NULL,
+  `TYPE` varchar(30) COLLATE utf8_bin DEFAULT NULL,
+  `UNIQUE` varchar(30) COLLATE utf8_bin DEFAULT NULL,
+  `TABLEID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`COLID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `colinfo` */
+
+insert  into `colinfo`(`COLID`,`NAME`,`TYPE`,`UNIQUE`,`TABLEID`) values 
+(1,'1',NULL,NULL,NULL);
 
 /*Table structure for table `dept` */
 
 DROP TABLE IF EXISTS `dept`;
 
 CREATE TABLE `dept` (
-  `dept_id` int(11) NOT NULL AUTO_INCREMENT,
-  `dept_name` varchar(30) COLLATE utf8_bin DEFAULT NULL,
-  `fdept_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`dept_id`)
+  `DEPT_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DEPT_NAME` varchar(30) COLLATE utf8_bin DEFAULT NULL,
+  `FDEPT_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`DEPT_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `dept` */
 
 /*Table structure for table `job` */
 
 DROP TABLE IF EXISTS `job`;
 
 CREATE TABLE `job` (
-  `job_id` int(11) NOT NULL AUTO_INCREMENT,
-  `job_name` varchar(30) COLLATE utf8_bin DEFAULT NULL,
-  `dept_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`job_id`)
+  `JOB_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `JOB_NAME` varchar(30) COLLATE utf8_bin DEFAULT NULL,
+  `DEPT_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`JOB_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `job` */
 
 /*Table structure for table `person` */
 
 DROP TABLE IF EXISTS `person`;
 
 CREATE TABLE `person` (
-  `person_id` int(11) NOT NULL AUTO_INCREMENT,
-  `p_name` varchar(30) COLLATE utf8_bin DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  `info` varchar(1000) COLLATE utf8_bin DEFAULT NULL,
-  `dept_id` int(11) DEFAULT NULL,
-  `job_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`person_id`)
+  `PERSON_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `P_NAME` varchar(30) COLLATE utf8_bin DEFAULT NULL,
+  `AGE` int(11) DEFAULT NULL,
+  `INFO` varchar(1000) COLLATE utf8_bin DEFAULT NULL,
+  `DEPT_ID` int(11) DEFAULT NULL,
+  `JOB_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`PERSON_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `person` */
 
 /*Table structure for table `tableinfo` */
 
 DROP TABLE IF EXISTS `tableinfo`;
 
 CREATE TABLE `tableinfo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(30) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `tableinfo` */
 
 /*Table structure for table `user` */
 
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `USER_ID` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) COLLATE utf8_bin DEFAULT NULL,
   `password` varchar(30) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`USER_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `user` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
